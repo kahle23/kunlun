51754a4fcb9dffb15db7949f839f9241f8cd48e3
Kahle <kahle@undefined>
2017-10-12 14:55:43 +0800

    saber 0.0.2-beta2

---
+++ src/test/java/sabertest/util/ReflectTest.java
@@ -0,0 +1,49 @@
+package sabertest.util;
+
+import org.junit.Test;
+import saber.util.Reflect;
+import saber.util.Time;
+
+import java.util.Calendar;
+
+public class ReflectTest {
+
+    private static final String TEST_VAL = "hello, world! ";
+    private final String TEST_VAL1 = "hello, world! ";
+
+    @Test
+    public void test1() throws Exception {
+        Reflect reflect = Reflect.on(Time.class);
+        Object call = reflect.call("on");
+        System.out.println(call);
+        Object calendar = reflect.setBean(call).get("calendar");
+        System.out.println(calendar instanceof Calendar);
+        System.out.println(Time.on(((Calendar) calendar).getTime()));
+    }
+
+    @Test
+    public void test2() throws Exception {
+        Reflect reflect = Reflect.on(Time.class);
+        System.out.println(reflect.setBean(reflect.call("on")).call("format"));
+        Time time = Time.on(1990, 1, 1, 12, 12, 12);
+        System.out.println(reflect.set("calendar", time.getCalendar()).call("format"));
+    }
+
+    @Test
+    public void test3() throws Exception {
+        Reflect reflect = Reflect.on(ReflectTest.class).create();
+        System.out.println(reflect.get("TEST_VAL"));
+        reflect.set("TEST_VAL", "123456");
+        System.out.println(reflect.get("TEST_VAL"));
+    }
+
+    @Test
+    public void test4() throws Exception {
+        Reflect reflect = Reflect.on(ReflectTest.class).create();
+        System.out.println(reflect.get("TEST_VAL1"));
+        reflect.set("TEST_VAL1", "123456");
+        System.out.println(reflect.get("TEST_VAL1"));
+        System.out.println(reflect.get("TEST_VAL"));
+    }
+
+}
