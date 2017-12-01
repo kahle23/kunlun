905f00d2a35728d4245f994fc9d0206e3d2208c4
Kahle <kahle@undefined>
2017-12-01 15:16:14 +0800

    artoria 0.0.5-beta3

---
+++ src/main/java/artoria/util/StringConst.java
@@ -0,0 +1,27 @@
+package artoria.util;
+
+/**
+ * @author Kahle
+ */
+public class StringConst {
+
+    public static final String ENDL = System.getProperty("line.separator");
+
+    public static final String PLUS = "+";
+    public static final String MINUS = "-";
+
+    public static final String DOT = ".";
+    public static final String EQUAL = "=";
+    public static final String COLON = ":";
+    public static final String COMMA = ",";
+    public static final String BLANK_SPACE = " ";
+    public static final String EMPTY_STRING = "";
+
+    public static final String LEFT_PARENTHESIS = "(";
+    public static final String RIGHT_PARENTHESIS = ")";
+    public static final String LEFT_SQUARE_BRACKET = "[";
+    public static final String RIGHT_SQUARE_BRACKET = "]";
+    public static final String LEFT_CURLY_BRACKET = "{";
+    public static final String RIGHT_CURLY_BRACKET = "}";
+
+}
