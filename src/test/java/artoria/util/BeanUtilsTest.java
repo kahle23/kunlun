a731ab5c48169f6dd07482de834f7c038927f74c
Kahle <Kahlkn@gmail.com>
2017-12-20 00:35:48 +0800

    artoria 0.0.5-beta14 Update BeanUtils

--- src/test/java/artoria/util/BeanUtilsTest.java
+++ src/test/java/artoria/util/BeanUtilsTest.java
@@ -1,12 +1,12 @@
 package artoria.util;
 
 import artoria.beans.Student;
+import artoria.exception.ReflectionException;
 import org.junit.Test;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Map;
-
-import static org.junit.Assert.*;
 
 public class BeanUtilsTest {
 
@@ -39,7 +39,20 @@ public class BeanUtilsTest {
     }
 
     @Test
-    public void copyProperties1() {
+    public void copyProperties1() throws ReflectionException {
+        Student student = new Student();
+        student.setName("zhangsan");
+        student.setAge(19);
+        student.setHeight(176d);
+        student.setScore(89);
+        student.setEmail("zhangsan@email.com");
+        Map<String, Object> map = new HashMap<String, Object>();
+        BeanUtils.copyProperties(student, map);
+        System.out.println(map);
+
+        Student student1 = new Student();
+        BeanUtils.copyProperties(map, student1);
+        System.out.println(student1);
     }
 
     @Test
@@ -48,7 +61,7 @@ public class BeanUtilsTest {
 
     @Test
     public void findAllGetterOrSetterMethods() {
-        Map<String, Method> methods = BeanUtils.findAllGetterOrSetterMethods(Student.class);
+        Map<String, Method> methods = BeanUtils.findAllGetOrSetMethods(Student.class);
         for (Map.Entry<String, Method> entry : methods.entrySet()) {
             System.out.println(entry.getKey() + " | " + entry.getValue().getName());
         }
