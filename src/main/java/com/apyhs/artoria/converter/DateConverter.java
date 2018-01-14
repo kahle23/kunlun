57e2b6d237f0863afb7db20e1982909b34c21d89
Kahle <kahle@undefined>
2018-01-15 00:22:20 +0800

    Update converter

--- src/main/java/com/apyhs/artoria/converter/DateConverter.java
+++ src/main/java/com/apyhs/artoria/converter/DateConverter.java
@@ -1,21 +1,16 @@
 package com.apyhs.artoria.converter;
 
-import com.apyhs.artoria.logging.Logger;
-import com.apyhs.artoria.logging.LoggerFactory;
 import com.apyhs.artoria.reflect.ReflectUtils;
 import com.apyhs.artoria.util.Assert;
 import com.apyhs.artoria.util.ClassUtils;
 
 import java.util.Date;
 
-import static com.apyhs.artoria.util.Const.COLON;
-
 /**
  * Date converter.
  * @author Kahle
  */
 public class DateConverter implements Converter {
-    private static final Logger log = LoggerFactory.getLogger(DateConverter.class);
 
     protected Object dateToDate(Object source, Class<?> target) {
         long time = ((Date) source).getTime();
@@ -32,7 +27,6 @@ public class DateConverter implements Converter {
         Assert.notNull(source, "Parameter \"source\" must not null. ");
         Assert.notNull(target, "Parameter \"target\" must not null. ");
         Class<?> clazz = source.getClass();
-        log.debug(DateConverter.class.getSimpleName() + COLON + clazz.getName() + " >> " + target.getName());
         target = ClassUtils.getWrapper(target);
         if (target.isAssignableFrom(clazz)) {
             return source;
