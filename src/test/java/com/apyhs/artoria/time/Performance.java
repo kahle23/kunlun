2f992373dafda3e04c8fd1fb0464cce49654fc81
Kahle <kahle@undefined>
2018-02-01 14:59:52 +0800

    Add ThreadLocalUtils

--- src/test/java/com/apyhs/artoria/time/Performance.java
+++ src/test/java/com/apyhs/artoria/time/Performance.java
@@ -1,19 +1,20 @@
 package com.apyhs.artoria.time;
 
+import com.apyhs.artoria.util.ThreadLocalUtils;
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Performance {
     private static final Calendar TEMPLATE = Calendar.getInstance();
+    private int count = 50;
+    private int createCount = 200000;
 
     @Test
-    public void _performanceDateAndCalendar() {
+    public void createDateAndCalendar() {
         System.out.println();
-
-        int count = 50;
-        int createCount = 200000;
 
         for (int i = 0; i < count; i++) {
             long start = System.currentTimeMillis();
@@ -46,6 +47,35 @@ public class Performance {
         }
         System.out.println();
 
+        System.out.println();
+    }
+
+    @Test
+    public void createSimpleDateFormat() {
+        for (int i = 0; i < count; i++) {
+            long start = System.currentTimeMillis();
+            for (int j = 0; j < createCount; j++) {
+                new SimpleDateFormat(DateUtils.DEFAULT_DATE_PATTERN);
+            }
+            long end = System.currentTimeMillis();
+            System.out.print((end - start) + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < count; i++) {
+            long start = System.currentTimeMillis();
+            for (int j = 0; j < createCount; j++) {
+                SimpleDateFormat dateFormat =
+                        (SimpleDateFormat) ThreadLocalUtils.getValue(DateUtils.DEFAULT_DATE_PATTERN);
+                if (dateFormat == null) {
+                    dateFormat =
+                            new SimpleDateFormat(DateUtils.DEFAULT_DATE_PATTERN);
+                    ThreadLocalUtils.setValue(DateUtils.DEFAULT_DATE_PATTERN, dateFormat);
+                }
+            }
+            long end = System.currentTimeMillis();
+            System.out.print((end - start) + " ");
+        }
         System.out.println();
     }
 
