33a32bcecdbd83503db36a63d0a9b42aaa2155c8
Kahle <Kahlkn@gmail.com>
2018-02-01 15:13:26 +0800

    Update ThreadUtils

--- src/test/java/com/apyhs/artoria/util/ThreadUtilsTest.java
+++ src/test/java/com/apyhs/artoria/util/ThreadUtilsTest.java
@@ -1,17 +1,18 @@
 package com.apyhs.artoria.util;
 
+import com.alibaba.fastjson.JSON;
 import org.junit.Test;
+
+import java.lang.management.ThreadInfo;
 
 public class ThreadUtilsTest {
 
     @Test
     public void test() {
         ThreadUtils.sleepQuietly(1000);
-        System.out.println(ThreadUtils.getLineNumber());
-        System.out.println(ThreadUtils.getMethodName());
-        System.out.println(ThreadUtils.getClassName());
         System.out.println(ThreadUtils.getThreadName());
-        System.out.println(ThreadUtils.getFileName());
+        ThreadInfo threadInfo = ThreadUtils.getThreadInfo(1);
+        System.out.println(JSON.toJSONString(threadInfo, true));
     }
 
 }
