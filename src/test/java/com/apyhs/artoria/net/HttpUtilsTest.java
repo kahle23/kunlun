0a56eaac60b2a811e3793d6e9eca5fef537083df
Kahle <Kahlkn@gmail.com>
2018-02-05 11:20:18 +0800

    Update HttpUtils

--- src/test/java/com/apyhs/artoria/net/HttpUtilsTest.java
+++ src/test/java/com/apyhs/artoria/net/HttpUtilsTest.java
@@ -1,56 +1,67 @@
 package com.apyhs.artoria.net;
 
+import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HttpUtilsTest {
+    private String testUrl = "https://apyhs.blog/";
+    private String testUrl1 = "https://www.taobao.com";
+    private String testUrl2 = "https://www.baidu.com";
 
     @Test
     public void test1() throws Exception {
-//        System.out.println(Http.on("http://uux.me").get());
-//        System.out.println(Http.on("https://www.taobao.com").get());
-        System.out.println(HttpUtils.create("https://www.baidu.com").get());
+        System.out.println(HttpUtils.create(testUrl).get());
+        System.out.println(HttpUtils.create(testUrl1).get());
+        System.out.println(HttpUtils.create(testUrl2).get());
     }
 
     @Test
     public void test2() throws Exception {
-        System.out.println(HttpUtils.create("http://uux.me").get());
-//        System.out.println(Http.on("http://uux.me").post());
-//        System.out.println(Http.on("http://uux.me").put());
-//        System.out.println(Http.on("http://uux.me").delete());
-//        System.out.println(Http.on("http://uux.me").head());
-//        System.out.println(Http.on("http://uux.me").trace());
-//        System.out.println(Http.on("http://uux.me").options());
-//        System.out.println(Http.on("http://uux.me").setMethod("GET").send());
+        System.out.println(HttpUtils.create(testUrl).get());
+        System.out.println(HttpUtils.create(testUrl).post());
+        System.out.println(HttpUtils.create(testUrl).put());
+        System.out.println(HttpUtils.create(testUrl).delete());
+        System.out.println(HttpUtils.create(testUrl).head());
+        System.out.println(HttpUtils.create(testUrl).options());
+        System.out.println(HttpUtils.create(testUrl).setMethod("GET").execute());
     }
 
     @Test
     public void test3() throws Exception {
         HttpUtils http = HttpUtils.create();
-        System.out.println(http.setUrl("http://uux.me").get());
-//        System.out.println(http.setUrl("https://www.baidu.com").get());
+        System.out.println(http.setUrl(testUrl).get());
         http.setConnectTimeout(1000).setReadTimeout(1000)
                 .setUserAgent(">>>>>>>>>>>>>>>>>>>>>").setCharset("utf-8");
-        System.out.println(http.setUrl("http://uux.me").get());
-        System.out.println(http.setUrl("https://www.baidu.com").get());
+        System.out.println(http.setUrl(testUrl1).get());
+        System.out.println(http.setUrl(testUrl2).get());
     }
 
     @Test
     public void test4() throws Exception {
-        HttpUtils http = HttpUtils.create().setConnectTimeout(1000)
-                .setReadTimeout(1000).setCharset("utf-8")
-                .setUserAgent(">>>>>>>>>>>>>>>>>>>>>");
-//        System.out.println(http.get("http://uux.me"));
-//        System.out.println(http.post("http://uux.me"));
-        System.out.println(http.send("http://uux.me", "GET"));
-    }
-
-    @Test
-    public void test5() throws Exception {
         String url = "http://ip.chinaz.com/getip.aspx";
         System.out.println(HttpUtils.create(url)
                 .setHttpProxy("127.0.0.1", 5000)
                 .setProxyAuth("zhangsan", "123")
                 .get());
+    }
+
+    // @Test
+    public void test5() throws Exception {
+        String url = "https://";
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put("labelName", "");
+        data.put("categoryId", "");
+        data.put("pageNum", "1");
+        data.put("pageSize", "-1");
+        String post = HttpUtils.create(url)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "")
+                .setData(JSON.toJSONString(data).getBytes())
+                .post();
+        System.out.println(JSON.toJSONString(JSON.parseObject(post), true));
     }
 
 }
