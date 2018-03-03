1ccde91ba20bea12bf4260bbeae47858c54814be
Kahle <Kahlkn@gmail.com>
2018-03-03 17:22:20 +0800

    Update serialize and codec

--- src/main/java/com/apyhs/artoria/codec/Unicode.java
+++ src/main/java/com/apyhs/artoria/codec/Unicode.java
@@ -7,9 +7,7 @@ import com.apyhs.artoria.util.Assert;
  * @author Kahle
  */
 public class Unicode {
-
     private static final String BACKLASH_U = "\\u";
-    private static final String REGEX_BACKLASH_U = "\\\\u";
     private static final int RADIX = 16;
 
     public static String encode(String string) {
@@ -18,6 +16,10 @@ public class Unicode {
         char[] chars = string.toCharArray();
         for (char c : chars) {
             String hexString = Integer.toHexString(c);
+            int len = hexString.length();
+            if (len != 4) {
+                hexString = (len == 2 ? "00" : "0") + hexString;
+            }
             unicode.append(BACKLASH_U).append(hexString);
         }
         return unicode.toString();
@@ -25,11 +27,16 @@ public class Unicode {
 
     public static String decode(String unicode) {
         Assert.notNull(unicode, "Unicode must is not null. ");
+        int index, pos = 0;
         StringBuilder result = new StringBuilder();
-        String[] hexs = unicode.split(REGEX_BACKLASH_U);
-        for (int i = 1; i < hexs.length; i++) {
-            int anInt = Integer.parseInt(hexs[i], RADIX);
-            result.append((char) anInt);
+        while ((index = unicode.indexOf(BACKLASH_U, pos)) != -1) {
+            result.append(unicode.substring(pos, index));
+            if (index + 5 < unicode.length()) {
+                pos = index + 6;
+                String hex = unicode.substring(index + 2, index + 6);
+                char ch = (char) Integer.parseInt(hex, RADIX);
+                result.append(ch);
+            }
         }
         return result.toString();
     }
