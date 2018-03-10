bb191ddbbb386e68fc1900760aa5d1c9a6f34eb6
Kahle <Kahlkn@gmail.com>
2018-03-10 17:36:09 +0800

    Add jdbc

---
+++ src/test/java/com/apyhs/artoria/entity/User.java
@@ -0,0 +1,59 @@
+package com.apyhs.artoria.entity;
+
+public class User {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String phone;
+    private String email;
+    private String blog;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBlog() {
+        return blog;
+    }
+
+    public void setBlog(String blog) {
+        this.blog = blog;
+    }
+
+}
