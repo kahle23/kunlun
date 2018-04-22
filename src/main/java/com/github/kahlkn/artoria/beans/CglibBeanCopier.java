6723aaf54793cd39d6e83022149c4e34495d1f46
Kahle <kahle@undefined>
2018-04-22 17:46:31 +0800

    Remove template engine utils and add cglib bean copier ignore properties

--- src/main/java/com/github/kahlkn/artoria/beans/CglibBeanCopier.java
+++ src/main/java/com/github/kahlkn/artoria/beans/CglibBeanCopier.java
@@ -2,8 +2,12 @@ package com.github.kahlkn.artoria.beans;
 
 import com.github.kahlkn.artoria.converter.Converter;
 import com.github.kahlkn.artoria.util.Assert;
+import com.github.kahlkn.artoria.util.CollectionUtils;
+import com.github.kahlkn.artoria.util.StringUtils;
 
 import java.util.List;
+
+import static com.github.kahlkn.artoria.util.Const.GET_OR_SET_LENGTH;
 
 /**
  * Cglib bean copier.
@@ -13,23 +17,31 @@ public class CglibBeanCopier implements BeanCopier {
 
     private static class CglibConverterAdapter implements net.sf.cglib.core.Converter {
         private Converter converter;
-
-        public Converter getConverter() {
-            return converter;
-        }
+        private List<String> ignoreProperties;
+        private boolean hasIgnore;
 
         public void setConverter(Converter converter) {
             Assert.notNull(converter, "Parameter \"converter\" must not null. ");
             this.converter = converter;
         }
 
-        public CglibConverterAdapter(Converter converter) {
+        public CglibConverterAdapter(Converter converter, List<String> ignoreProperties) {
             this.setConverter(converter);
+            this.ignoreProperties = ignoreProperties;
+            this.hasIgnore = CollectionUtils.isNotEmpty(ignoreProperties);
         }
 
         @Override
-        public Object convert(Object o, Class aClass, Object o1) {
-            return converter.convert(o, aClass);
+        public Object convert(Object value, Class valClass, Object methodName) {
+            if (hasIgnore) {
+                String tmp = (String) methodName;
+                tmp = tmp.substring(GET_OR_SET_LENGTH);
+                tmp = StringUtils.uncapitalize(tmp);
+                if (ignoreProperties.contains(tmp)) {
+                    return null;
+                }
+            }
+            return converter.convert(value, valClass);
         }
 
     }
@@ -39,7 +51,7 @@ public class CglibBeanCopier implements BeanCopier {
         Class<?> fromClass = from.getClass();
         Class<?> toClass = to.getClass();
         net.sf.cglib.beans.BeanCopier copier = net.sf.cglib.beans.BeanCopier.create(fromClass, toClass, true);
-        CglibConverterAdapter adapter = new CglibConverterAdapter(converter);
+        CglibConverterAdapter adapter = new CglibConverterAdapter(converter, ignoreProperties);
         copier.copy(from, to, adapter);
     }
 
