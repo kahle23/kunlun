9d8dcf6fa798ff1c82ae309faf2a4e2c268e292a
Kahle <kahle@undefined>
2018-10-24 16:53:50 +0800

    Add lifecycle tools and update jdbc tools

---
+++ src/main/java/artoria/lifecycle/LifecycleUtils.java
@@ -0,0 +1,71 @@
+package artoria.lifecycle;
+
+import artoria.util.CollectionUtils;
+
+import java.util.Collection;
+import java.util.logging.Logger;
+
+/**
+ * Help call {@link Initializable#initialize()} or {@link Destroyable#destroy()}.
+ * @author Kahle
+ */
+public class LifecycleUtils {
+    private static Logger log = Logger.getLogger(LifecycleUtils.class.getName());
+
+    public static void initialize(Object obj) throws LifecycleException {
+        if (obj instanceof Initializable) {
+            LifecycleUtils.initialize((Initializable) obj);
+        }
+        else if (obj instanceof Collection) {
+            LifecycleUtils.initialize((Collection) obj);
+        }
+    }
+
+    public static void initialize(Collection collection) throws LifecycleException {
+        if (CollectionUtils.isEmpty(collection)) {
+            return;
+        }
+        for (Object obj : collection) {
+            LifecycleUtils.initialize(obj);
+        }
+    }
+
+    public static void initialize(Initializable initializable) throws LifecycleException {
+        if (initializable == null) {
+            return;
+        }
+        initializable.initialize();
+    }
+
+    public static void destroy(Object obj) {
+        if (obj instanceof Destroyable) {
+            LifecycleUtils.destroy((Destroyable) obj);
+        }
+        else if (obj instanceof Collection) {
+            LifecycleUtils.destroy((Collection) obj);
+        }
+    }
+
+    public static void destroy(Collection collection) {
+        if (CollectionUtils.isEmpty(collection)) {
+            return;
+        }
+        for (Object obj : collection) {
+            LifecycleUtils.destroy(obj);
+        }
+    }
+
+    public static void destroy(Destroyable destroyable) {
+        if (destroyable == null) {
+            return;
+        }
+        try {
+            destroyable.destroy();
+        }
+        catch (Throwable t) {
+            log.info("Unable to cleanly destroy instance ["
+                    + destroyable + "] of type [" + destroyable.getClass().getName() + "]. ");
+        }
+    }
+
+}
