cdfd82e8bf4273904a8dae07919c060cd92e6abf
Kahle <kahle@undefined>
2021-01-03 15:43:38 +0800

    Add code related to key and value pair

---
+++ src/main/java/artoria/lang/KeyValue.java
@@ -0,0 +1,23 @@
+package artoria.lang;
+
+/**
+ * A key value pair consisting of two elements.
+ * @param <K> The key type
+ * @param <V> The value type
+ * @author Kahle
+ */
+public interface KeyValue<K, V> {
+
+    /**
+     * Gets the key from this key-value.
+     * @return The key from this key-value
+     */
+    K getKey();
+
+    /**
+     * Gets the value from this key-value.
+     * @return The value from this key-value
+     */
+    V getValue();
+
+}
