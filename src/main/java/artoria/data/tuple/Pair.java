564f672ac58aca479bb1acef71b5351e01d65479
Kahle <kahle@undefined>
2023-08-12 15:39:50 +0800

    Add the tuple related code

---
+++ src/main/java/artoria/data/tuple/Pair.java
@@ -0,0 +1,23 @@
+package artoria.data.tuple;
+
+/**
+ * A pair consisting of two elements.
+ * @param <L> The left element type
+ * @param <R> The right element type
+ * @author Kahle
+ */
+public interface Pair<L, R> {
+
+    /**
+     * Gets the left element from this pair.
+     * @return The left element, may be null
+     */
+    L getLeft();
+
+    /**
+     * Gets the right element from this pair.
+     * @return The right element, may be null
+     */
+    R getRight();
+
+}
