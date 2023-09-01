b87431aca83eae9f14c92611997939fe6f3dc2f6
Kahle <kahle@undefined>
2023-09-01 23:39:55 +0800

    Add data fields filtering handler

---
+++ src/main/java/artoria/core/handler/DataFieldsFilteringHandler.java
@@ -0,0 +1,22 @@
+package artoria.core.handler;
+
+import artoria.core.Handler;
+
+/**
+ * The data fields filtering handler.
+ * It is generally used for the processing of the result of the invoke
+ *      (usually in the case of data permissions).
+ * @author Kahle
+ */
+public interface DataFieldsFilteringHandler extends Handler {
+
+    /**
+     * Filter data fields.
+     * @param type It is used to distinguish fields filtering in different scenarios
+     * @param data The data to be filtered
+     * @param arguments The arguments that may be used during filtering
+     * @return The filtered data
+     */
+    Object filter(Object type, Object data, Object... arguments);
+
+}
