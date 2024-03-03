c7612f935595780c80130d943aec0b3673f17b39
Kahle <kahle@undefined>
2024-03-03 12:16:37 +0800

    [feat] Add the script based jdbc invoke action handler

---
+++ src/main/java/kunlun/action/support/jdbc/AbstractScriptBasedJdbcInvokeHandler.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2018. the original author or authors.
+ * Kunlun is licensed under the "LICENSE" file in the project's root directory.
+ */
+
+package kunlun.action.support.jdbc;
+
+import kunlun.action.support.AbstractScriptBasedInvokeHandler;
+import kunlun.data.Dict;
+import kunlun.data.bean.BeanUtils;
+import kunlun.data.validation.support.ValidationConfig;
+import kunlun.logging.Logger;
+import kunlun.logging.LoggerFactory;
+import kunlun.util.ClassUtils;
+import kunlun.util.StringUtils;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * The abstract script based jdbc invoke action handler.
+ * @author Kahle
+ */
+public abstract class AbstractScriptBasedJdbcInvokeHandler extends AbstractScriptBasedInvokeHandler {
+    private static final Logger log = LoggerFactory.getLogger(AbstractScriptBasedJdbcInvokeHandler.class);
+
+    @Override
+    protected void validateInput(InvokeContext context) {
+        JdbcInvokeConfig config = (JdbcInvokeConfig) context.getConfig();
+        Collection<ValidationConfig> validations = config.getInputValidations();
+        validate(config.getScriptEngine(), context, validations);
+    }
+
+    @Override
+    protected void convertInput(InvokeContext context) {
+        // Get config.
+        JdbcInvokeConfig config = (JdbcInvokeConfig) context.getConfig();
+        String scriptEngine = config.getScriptEngine();
+        String inputScript = config.getInput();
+        // Eval input script.
+        if (StringUtils.isNotBlank(inputScript)) {
+            Object convertedInput = eval(scriptEngine, inputScript, context);
+            context.setConvertedInput(convertedInput);
+        }
+        else { context.setConvertedInput(context.getRawInput()); }
+        // End.
+    }
+
+    @Override
+    protected void validateOutput(InvokeContext context) {
+        JdbcInvokeConfig config = (JdbcInvokeConfig) context.getConfig();
+        Collection<ValidationConfig> validations = config.getOutputValidations();
+        validate(config.getScriptEngine(), context, validations);
+    }
+
+    @Override
+    protected void convertOutput(InvokeContext context) {
+        // Get config and get script engine name.
+        JdbcInvokeConfig config = (JdbcInvokeConfig) context.getConfig();
+        Class<?> expectedClass = context.getExpectedClass();
+        String   scriptEngine = config.getScriptEngine();
+        String   outputScript = config.getOutput();
+        // Eval output script.
+        if (StringUtils.isNotBlank(outputScript)) {
+            Object convertedOutput = eval(scriptEngine, outputScript, context);
+            context.setConvertedOutput(convertedOutput);
+        }
+        else { context.setConvertedOutput(context.getRawOutput()); }
+        // Convert converted output.
+        Object convertedOutput = context.getConvertedOutput();
+        if (!ClassUtils.isSimpleValueType(expectedClass)) {
+            if (convertedOutput instanceof Collection) {
+            }
+            else if (convertedOutput!=null&&convertedOutput.getClass().isArray()) {
+            }
+            else if (convertedOutput instanceof Map && Map.class.isAssignableFrom(expectedClass)) {
+                context.setConvertedOutput(Dict.of((Map<?, ?>) convertedOutput));
+            }
+            else if (convertedOutput != null && !expectedClass.isAssignableFrom(convertedOutput.getClass())) {
+                context.setConvertedOutput(BeanUtils.beanToBean(convertedOutput, expectedClass));
+            }
+            else { /* Do nothing. */ }
+        }
+        // End.
+    }
+
+}
---
+++ src/main/java/kunlun/action/support/jdbc/JdbcInvokeConfig.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright (c) 2018. the original author or authors.
+ * Kunlun is licensed under the "LICENSE" file in the project's root directory.
+ */
+
+package kunlun.action.support.jdbc;
+
+import kunlun.data.validation.support.ValidationConfig;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * The jdbc invoke configuration.
+ * @author Kahle
+ */
+public class JdbcInvokeConfig implements Serializable {
+    /**
+     * The script engine name.
+     */
+    private String scriptEngine;
+    /**
+     * The text renderer name.
+     */
+    private String rendererName;
+    /**
+     * The input data validation configs.
+     */
+    private Collection<ValidationConfig> inputValidations;
+    /**
+     * The jdbc input conversion script.
+     */
+    private String input;
+    /**
+     * The jdbc execute type: 0 unknown, 1 nothing, 2 update (insert or update), 3 batch update (insert or update)
+     *  , 4 single value query, 5 single object query, 6 object array query, 7 object array paging query (page number)
+     *  , 8 object array paging query (scroll id)
+     */
+    private Integer executeType;
+    /**
+     * The jdbc execute sql.
+     */
+    private String sql;
+    /**
+     * The jdbc output conversion script.
+     */
+    private String output;
+    /**
+     * The output data validation configs.
+     */
+    private Collection<ValidationConfig> outputValidations;
+    /**
+     * The cache configuration.
+     * Mainly includes "cacheName" and "cacheKey".
+     */
+    private Map<String, String> cacheConfig;
+    /**
+     * The other configs.
+     */
+    private Map<Object, Object> otherConfigs;
+
+    public String getScriptEngine() {
+
+        return scriptEngine;
+    }
+
+    public void setScriptEngine(String scriptEngine) {
+
+        this.scriptEngine = scriptEngine;
+    }
+
+    public String getRendererName() {
+
+        return rendererName;
+    }
+
+    public void setRendererName(String rendererName) {
+
+        this.rendererName = rendererName;
+    }
+
+    public Collection<ValidationConfig> getInputValidations() {
+
+        return inputValidations;
+    }
+
+    public void setInputValidations(Collection<ValidationConfig> inputValidations) {
+
+        this.inputValidations = inputValidations;
+    }
+
+    public String getInput() {
+
+        return input;
+    }
+
+    public void setInput(String input) {
+
+        this.input = input;
+    }
+
+    public Integer getExecuteType() {
+
+        return executeType;
+    }
+
+    public void setExecuteType(Integer executeType) {
+
+        this.executeType = executeType;
+    }
+
+    public String getSql() {
+
+        return sql;
+    }
+
+    public void setSql(String sql) {
+
+        this.sql = sql;
+    }
+
+    public String getOutput() {
+
+        return output;
+    }
+
+    public void setOutput(String output) {
+
+        this.output = output;
+    }
+
+    public Collection<ValidationConfig> getOutputValidations() {
+
+        return outputValidations;
+    }
+
+    public void setOutputValidations(Collection<ValidationConfig> outputValidations) {
+
+        this.outputValidations = outputValidations;
+    }
+
+    public Map<String, String> getCacheConfig() {
+
+        return cacheConfig;
+    }
+
+    public void setCacheConfig(Map<String, String> cacheConfig) {
+
+        this.cacheConfig = cacheConfig;
+    }
+
+    public Map<Object, Object> getOtherConfigs() {
+
+        return otherConfigs;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void setOtherConfigs(Map<?, ?> otherConfigs) {
+
+        this.otherConfigs = (Map<Object, Object>) otherConfigs;
+    }
+
+}
