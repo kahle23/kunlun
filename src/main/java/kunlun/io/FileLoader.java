3998ba954d5294be9250a21f9de2c4ba10d0364b
Kahle <kahle@undefined>
2024-03-10 19:18:18 +0800

    [feat] Add the unified interface "FileLoader" and its implementation simple file loader

---
+++ src/main/java/kunlun/io/FileLoader.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018. the original author or authors.
+ * Kunlun is licensed under the "LICENSE" file in the project's root directory.
+ */
+
+package kunlun.io;
+
+import kunlun.core.Loader;
+
+import java.io.InputStream;
+
+public interface FileLoader extends Loader<Object, InputStream> {
+}
---
+++ src/main/java/kunlun/io/file/support/SimpleFileLoader.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2018. the original author or authors.
+ * Kunlun is licensed under the "LICENSE" file in the project's root directory.
+ */
+
+package kunlun.io.file.support;
+
+import kunlun.exception.ExceptionUtils;
+import kunlun.io.FileLoader;
+import kunlun.util.Assert;
+import kunlun.util.ClassLoaderUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SimpleFileLoader implements FileLoader {
+    protected static final String CLASSPATH = "classpath:";
+
+    @Override
+    public InputStream load(Object param) {
+        Assert.notNull(param, "Parameter \"param\" must not null. ");
+        String filePath = String.valueOf(param);
+        Assert.notNull(filePath, "Variable \"filePath\" must not blank. ");
+        try {
+            return filePath.startsWith(CLASSPATH) ?
+                    ClassLoaderUtils.getResourceAsStream(
+                            filePath.substring(CLASSPATH.length()), this.getClass()
+                    ) :
+                    new FileInputStream(filePath);
+        }
+        catch (IOException e) { throw ExceptionUtils.wrap(e); }
+    }
+
+}
